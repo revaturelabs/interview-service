adde23d952f0a234b68a3b021ee6470e1eeab872
John Kelly <JohnThaddeusKelly@outlook.com>
2019-11-21 13:13:14 -0500

    All Green

--- src/test/java/com/revature/InterviewServiceApplicationTests.java
+++ src/test/java/com/revature/InterviewServiceApplicationTests.java
@@ -13,14 +13,20 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.revature.model.Interview;
 import com.revature.model.Job;
+import com.revature.model.Profile;
+import com.revature.model.Skill;
+import com.revature.repository.InterviewRepository;
 import com.revature.repository.JobRepository;
+import com.revature.repository.ProfileRepository;
+import com.revature.repository.SkillRepository;
 
 /*	Integration testing for the Job Repository
  * @author John Thaddeus Kelly
  */
 
-@SpringBootTest(classes = {JobRepository.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(classes = {JobRepository.class, SkillRepository.class, ProfileRepository.class, InterviewRepository.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @RunWith(SpringRunner.class)
 @EnableAutoConfiguration
 public class InterviewServiceApplicationTests {
@@ -29,11 +35,35 @@ public class InterviewServiceApplicationTests {
 	
 	@Autowired
 	private JobRepository jobRepository;
+	@Autowired
+	private SkillRepository skillRepository;
+	@Autowired
+	private ProfileRepository profileRepository;
+	@Autowired
+	private InterviewRepository interviewRepository;
 	
 	@Test
-	public void whenFindAll_thenReturn() {
+	public void jobWhenFindAll_thenReturn() {
 		
 		List<Job> found = (List<Job>) jobRepository.findAll();
+		
+		assertNotNull(found);
+	}
+	@Test
+	public void skillWhenFindAll_thenReturn() {
+		List<Skill> found = (List<Skill>) skillRepository.findAll();
+		
+		assertNotNull(found);
+	}
+	@Test
+	public void profileWhenFindAll_thenReturn() {
+		List<Profile> found = (List<Profile>) profileRepository.findAll();
+		
+		assertNotNull(found);
+	}
+	@Test
+	public void interviewWhenFindAll_thenReturn() {
+		List<Interview> found = (List<Interview>) interviewRepository.findAll();
 		
 		assertNotNull(found);
 	}
