b8f7236ac75b94733fb5e729bc96139e5fa247b8
Davin-Merry <dmerry@garcat.com>
2019-12-02 19:01:57 -0500

    Removed hard-coded URL

--- src/main/java/com/revature/controllers/AuthInterface.java
+++ src/main/java/com/revature/controllers/AuthInterface.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name="User-Service", url="http://localhost:8765/user-service")
+@FeignClient(name="User-Service")
 public interface AuthInterface {
 	@PostMapping("/users/authorize")
 	public boolean authorize(@RequestHeader(name = "auth") String token);
