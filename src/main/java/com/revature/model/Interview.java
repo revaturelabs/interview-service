a9bf6ba45d5ce4afedbfb86412716139f45bd979
El Guapo <williamalexmorgan@outlook.com>
2019-12-10 10:04:23 -0600

    gettersandsettersworknow

--- src/main/java/com/revature/model/Interview.java
+++ src/main/java/com/revature/model/Interview.java
@@ -1,9 +1,8 @@
 package com.revature.model;
 
 import java.sql.Timestamp;
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
@@ -17,7 +16,6 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 /**
@@ -56,12 +54,11 @@ public class Interview {
 	// @JsonIgnore
 	private Set<User> users = new HashSet<>();
 
+
 	public Interview() {
 	}
 
-	public Interview(int id, Profile profile, List<Comment> comments, Timestamp date, boolean isComplete, Job job,
-			Set<User> users) {
-		super();
+	public Interview(int id, Profile profile, Timestamp date, boolean isComplete, Job job, Set<User> users) {
 		this.id = id;
 		this.profile = profile;
 		this.date = date;
@@ -69,61 +66,6 @@ public class Interview {
 		this.job = job;
 		this.users = users;
 	}
-
-	// @Override
-	// public int hashCode() {
-	// final int prime = 31;
-	// int result = 1;
-	// result = prime * result + ((comments == null) ? 0 : comments.hashCode());
-	// result = prime * result + ((date == null) ? 0 : date.hashCode());
-	// result = prime * result + id;
-	// result = prime * result + ((users == null) ? 0 : users.hashCode());
-	// result = prime * result + (isComplete ? 1231 : 1237);
-	// result = prime * result + ((job == null) ? 0 : job.hashCode());
-	// result = prime * result + ((profile == null) ? 0 : profile.hashCode());
-	// return result;
-	// }
-	//
-	// @Override
-	// public boolean equals(Object obj) {
-	// if (this == obj)
-	// return true;
-	// if (obj == null)
-	// return false;
-	// if (getClass() != obj.getClass())
-	// return false;
-	// Interview other = (Interview) obj;
-	// if (comments == null) {
-	// if (other.comments != null)
-	// return false;
-	// } else if (!comments.equals(other.comments))
-	// return false;
-	// if (date == null) {
-	// if (other.date != null)
-	// return false;
-	// } else if (!date.equals(other.date))
-	// return false;
-	// if (id != other.id)
-	// return false;
-	// if (users == null) {
-	// if (other.users != null)
-	// return false;
-	// } else if (!users.equals(other.users))
-	// return false;
-	// if (isComplete != other.isComplete)
-	// return false;
-	// if (job == null) {
-	// if (other.job != null)
-	// return false;
-	// } else if (!job.equals(other.job))
-	// return false;
-	// if (profile == null) {
-	// if (other.profile != null)
-	// return false;
-	// } else if (!profile.equals(other.profile))
-	// return false;
-	// return true;
-	// }
 
 	public int getId() {
 		return this.id;
@@ -170,18 +112,69 @@ public class Interview {
 	}
 
 	public Set<User> getUsers() {
-		return users;
+		return this.users;
 	}
 
 	public void setUsers(Set<User> users) {
 		this.users = users;
 	}
 
-	public List<Comment> getComments() {
-		return comments;
+	public Interview id(int id) {
+		this.id = id;
+		return this;
 	}
 
-	public void setComments(List<Comment> comments) {
-		this.comments = comments;
+	public Interview profile(Profile profile) {
+		this.profile = profile;
+		return this;
 	}
+
+	public Interview date(Timestamp date) {
+		this.date = date;
+		return this;
+	}
+
+	public Interview isComplete(boolean isComplete) {
+		this.isComplete = isComplete;
+		return this;
+	}
+
+	public Interview job(Job job) {
+		this.job = job;
+		return this;
+	}
+
+	public Interview users(Set<User> users) {
+		this.users = users;
+		return this;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (o == this)
+			return true;
+		if (!(o instanceof Interview)) {
+			return false;
+		}
+		Interview interview = (Interview) o;
+		return id == interview.id && Objects.equals(profile, interview.profile) && Objects.equals(date, interview.date) && isComplete == interview.isComplete && Objects.equals(job, interview.job) && Objects.equals(users, interview.users);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, profile, date, isComplete, job, users);
+	}
+
+	@Override
+	public String toString() {
+		return "{" +
+			" id='" + getId() + "'" +
+			", profile='" + getProfile() + "'" +
+			", date='" + getDate() + "'" +
+			", isComplete='" + isIsComplete() + "'" +
+			", job='" + getJob() + "'" +
+			", users='" + getUsers() + "'" +
+			"}";
+	}
+	
 }
