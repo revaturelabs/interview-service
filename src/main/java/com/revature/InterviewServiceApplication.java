6dff1f467fc23c15aa35a9c9ce4a9ed944fa10a4
Ryan Kim <ryankeehoon@gmail.com>
2020-01-09 12:00:15 -0500

    commit
    1) fixed connectivity to eureka (service-registry) by adding three lines on application.properties.
    2) fixed cors issue by adding @Bean corsFilter() method in maindriver

--- src/main/java/com/revature/InterviewServiceApplication.java
+++ src/main/java/com/revature/InterviewServiceApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -14,4 +18,19 @@ public class InterviewServiceApplication {
 		SpringApplication.run(InterviewServiceApplication.class, args);
 	}
 
+	@Bean
+	public CorsFilter corsFilter() {
+	    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	    CorsConfiguration config = new CorsConfiguration();
+	    config.setAllowCredentials(true);
+	    config.addAllowedOrigin("*");
+	    config.addAllowedHeader("*");
+	    config.addAllowedMethod("*");
+	    source.registerCorsConfiguration("/**", config);
+	    
+	    System.out.println("got to bean cors filter");
+	    
+	    return new CorsFilter(source);
+	    
+	}	
 }
