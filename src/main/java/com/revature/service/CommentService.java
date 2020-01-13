2f9efa309e249774f69c43d2a13d3b7caaafffb1
Eric Delaney <ericmdelaney@gmail.com>
2020-01-13 10:31:17 -0500

    Backend name refactoring
    Refactored names in backend. No business logic changed. Accidently named branch without 1911, so this branch was built off of backend-name-refactoring.

--- src/main/java/com/revature/service/CommentService.java
+++ src/main/java/com/revature/service/CommentService.java
@@ -10,6 +10,7 @@ import com.revature.repository.InterviewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+
 @Service
 public class CommentService {
 
