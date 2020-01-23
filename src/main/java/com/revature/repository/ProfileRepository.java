61d062eb038d6712ba1f4c389507a4107519a539
Miranda_Brawner <mirandawbrawner@gmail.com>
2020-01-22 20:38:19 -0500

    Added javadocs for ProfileRepository.java
    Added javadocs for all methods in the ProfileRepository interface.

--- src/main/java/com/revature/repository/ProfileRepository.java
+++ src/main/java/com/revature/repository/ProfileRepository.java
@@ -8,23 +8,44 @@ import org.springframework.stereotype.Repository;
 
 import com.revature.model.Profile;
 
-/**
- * Representation of the profile repository
- * 
+/** Representation of the profile repository
  * @author Adriana Long
- * @author Brian Abajah
- */
-
+ * @author Brian Abajah */
 @Repository
 public interface ProfileRepository extends JpaRepository<Profile, Integer> {
+	
+	/** Retrieves the profile of the first candidate in the database whose last name matches the provided name.
+	 Returns null if no such candidate is found.
+	 * @param lastName The last name of the candidate.
+	 * @return The profile of the first candidate in the database whose last name matches the provided name. */
     Profile findByLastName(String lastName);
 
+    /** Retrieves the profile of the first candidate in the database whose first name matches the provided name.
+     Returns null if no such candidate is found.
+     * @param firstName The first name of the candidate. 
+     * @return The profile of the first candidate in the database whose first name matches the provided name. */
     Profile findByFirstName(String firstName);
 
+    /** Retrieves the profile of the candidate in the database whose ID matches the provided number.
+     Returns null if no such candidate is found.
+     * @param id The integer that uniquely identifies the candidate. 
+     * @return The profile of the candidate in the database whose ID matches the provided number, 
+     or null if no such candidate is found. */
     Profile findById(int id);
 
+    /** Retrieves a list containing the profiles for all candidates whose last names match the provided string.
+     * @param lastName The last name of the candidates.
+     * @return A list containing the profiles for all candidates whose last names match the provided string. */
     List<Profile> findByLastNameOrderByLastNameAsc(String lastName);
 
+    /** Retrieves a list containing the profiles for all candidates on a given page whose first name starts with
+     a provided string, and whose last name starts with another provided string.
+     * @param firstName A string that will filter out all candidates whose first names do not start with that string.
+     * @param lastName A string that will filter out all candidates whose last names do not start with that string.
+     * @param page The page that will be searched for matching candidates. Any candidate outside this page will
+	 not be returned in the results list, even if they fulfill the other search criteria.
+     * @return A list containing the profiles for all candidates on a given page whose first name starts with
+     a provided string, and whose last name starts with another provided string. */
     List<Profile> findByFirstNameStartsWithIgnoreCaseAndLastNameStartsWithIgnoreCase(String firstName, String lastName, Pageable page);
 	
 }
