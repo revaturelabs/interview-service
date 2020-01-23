45cd74f7d6abf2676fb78d531709a8f8524466a6
Miranda_Brawner <mirandawbrawner@gmail.com>
2020-01-22 21:01:27 -0500

    Added javadocs to UserRepository.java
    Added javadocs for all methods in the UserRepository interface, and reformatted the opening javadoc.

--- src/main/java/com/revature/repository/UserRepository.java
+++ src/main/java/com/revature/repository/UserRepository.java
@@ -6,19 +6,22 @@ import org.springframework.stereotype.Repository;
 
 import com.revature.model.User;
 
-/**
- * 
- * @author Brittany Tinnin and Will Liederer
- * 
- *         <p>
- *         The custom User Repository interface extends the Crud Repository
- *         Interface for generic CRUD operation
- *         </p>
- */
-
+/** The custom User Repository interface extends the Crud Repository interface for generic CRUD operation
+ * @author Brittany Tinnin and Will Liederer */
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+	
+	/** Retrieves the interviewer from the database whose username matches the provided string.
+	 Returns null if no such interviewer is found.
+	 * @param username The interviewer's username.
+	 * @return The interviewer from the database whose username matches the provided string,
+	 or null if no such interviewer is found. */
 	User findByUsername(String username);
 
+	/** Retrieves the interviewer from the database whose ID matches the provided number.
+	 Returns null if no such interviewer is found.
+	 * @param id The integer that uniquely identifies the interviewer.
+	 * @return The interviewer from the database whose ID matches the provided number,
+	 or null if no such interviewer is found. */
 	User findById(int id);
 }
