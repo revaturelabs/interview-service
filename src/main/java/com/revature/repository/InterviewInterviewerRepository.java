db89f38cffd1c1aff5bc910d70d5b913ce8cb05d
Sophia <msophiagavrila@gmail.com>
2020-01-31 15:17:59 -0500

    added InterviewInterviewerRepository and methods

---
+++ src/main/java/com/revature/repository/InterviewInterviewerRepository.java
@@ -0,0 +1,17 @@
+package com.revature.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.revature.model.Interview;
+import com.revature.model.InterviewInterviewer;
+
+@Repository
+public interface InterviewInterviewerRepository extends JpaRepository <InterviewInterviewer, Integer> {
+
+	List<InterviewInterviewer> findByInterview(Interview interview);
+	
+	List<InterviewInterviewer> findByInterviewer(String interviewer);
+}
