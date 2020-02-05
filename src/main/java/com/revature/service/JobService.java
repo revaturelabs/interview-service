85f5ee7c2dd16de9a73eda0eda614530ff9c5d33
Sophia <msophiagavrila@gmail.com>
2020-02-05 12:02:06 -0500

    modified CommentControllerTests & InterviewControllerTests

--- src/main/java/com/revature/service/JobService.java
+++ src/main/java/com/revature/service/JobService.java
@@ -1,10 +1,7 @@
 package com.revature.service;
 
 import java.util.List;
-<<<<<<< HEAD
-=======
 
->>>>>>> 677af72c655f196c2ad02172ff759364f48e82db
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
