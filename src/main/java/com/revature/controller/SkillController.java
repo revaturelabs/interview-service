4ad612c3b64a41df33e76ccd8820515c7c4a4daa
GattieTime <gattietime@gmail.com>
2020-02-11 16:13:59 -0500

    removed test function

--- src/main/java/com/revature/controller/SkillController.java
+++ src/main/java/com/revature/controller/SkillController.java
@@ -47,7 +47,7 @@ public class SkillController {
 	 * @author Seacriest Brown
 	 * @return List of skills
 	 */
-	@GetMapping
+	@GetMapping(value = "/allSkills")
 	public @ResponseBody List<Skill> allSkills() {
 		List<Skill> list = new ArrayList<>();
 		for (Skill skill : skillRepository.findAll()) {
@@ -62,7 +62,7 @@ public class SkillController {
 	 * @return The skill that matches the id
 	 * @author Seacriest Brown
 	 */
-	@GetMapping(value = "/{id}")
+	@GetMapping(value = "/getskillbyid/{id}")
 	public Skill getBySkill(@PathVariable("id") int id) {
 		return skillService.getById(id);
 	}
@@ -71,23 +71,32 @@ public class SkillController {
 	 * @author Joseph F Davis
 	 * @param skill The skill to insert
 	 */
-	@PostMapping
+	@PostMapping(value = "/insertSkill")
 	public boolean insertSkill(@RequestBody Skill skill) {
 		return skillService.insertSkill(skill);
 	}
 
+	/**
+	 * @author Joseph F Davis
+	 * @return All skills through the service layer
+	 */
+  
+	/*
+	 * @GetMapping(value = "/getSkills") public Iterable<Skill> getSkills() { return
+	 * sk.getAll(); }
+	 */
 
 	/**
 	 * @author Joseph F Davis
 	 * @param title
 	 * @return skill by title
 	 */
-	@GetMapping(value = "/title/{Title}")
+	@GetMapping(value = "/getskill/{Title}")
 	public List<Skill> getSkills(@PathVariable("Title") String title) {
 		return skillService.findSkill(title);
 	}
 
-	@GetMapping(value = "/title/{Title}/{page}")
+	@GetMapping(value = "/getskill/{Title}/{page}")
 	public List<Skill> getSkills(@PathVariable("Title") String title, @PathVariable int page) {
 		return skillService.findSkillPaged(title, page);
 	}
@@ -132,8 +141,4 @@ public class SkillController {
 		}
 	}
 	
-	@GetMapping(value = "/test")
-	public Skill test() {
-		throw new NullPointerException();
-	}
 }
